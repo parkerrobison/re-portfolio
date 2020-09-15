eb7104d86c30c4c0eda7663a14816d543d78b532
probison <parker.robison@hotmail.com>
2020-09-14 19:32:54 -0600

    fixed issues on the about component

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import portrait2 from '../../assets/images/portrait/portrait2.JPG'
 
-// WHEN I am presented with the About Me section
-// THEN I see a recent photo of the developer and a short bio about them
-
 function About() {
     return(
         <section className='my-5'>
@@ -13,9 +10,7 @@ function About() {
             </div>
             <p>
                 Hello! My name is Parker Robison. I graduated from USU with a Bachelors degree in Kinesiology. In desiring to further my education and expand my skills,
-                I enrolled in the University of Utah's coding bootcamp. I'm fairly new to development but I'm excited to learn more! I am familiar with 
-                HTML, CSS, Javascript, JQuery, Bootstrap, APIs and Materialize and I'll learn many other skills in the upcoming months. Currently I work as a Software Tester, but I hope to be a devleoper soon.
-                When I'm not learning about software development you can usually find me outside. Some of my hobbies include hiking, rock climbing, pickleball and photography.
+                I enrolled in the University of Utah's coding bootcamp. As a full stack developer, I'm focused on developing inspiring apps and websites. 
             </p>
         </section>
     )
